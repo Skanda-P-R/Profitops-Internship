9afcd2ba328e8409c60c9596b4d0fd59662cde34
broCode <56446798+PRASHANT-tech870@users.noreply.github.com>
2024-03-07 00:38:16 +0530

    Update configure.sh

--- AWS/Bash Approach(with admin access)/configure.sh
+++ AWS/Bash Approach(with admin access)/configure.sh
@@ -1,14 +1,6 @@
 ACCESS_KEY=""
 SECRET_KEY=""
 DEFAULT_REGION=""
-S3_ENDPOINT=""
-BUCKET_TEMPLATE=""
-ENCRYPTION_PASSWORD=""
-GPG_PROGRAM=""
-USE_HTTPS="" #yes/no
-HTTP_PROXY_NAME=""
-TEST_CASE="" #y/n
-SAVE_SETTINGS="" #y/n
-RETRY_CONFIGURATION=""  #y/n
+OUTPUT_FORMAT=""
 
 #specify your access key, secret key and default region
