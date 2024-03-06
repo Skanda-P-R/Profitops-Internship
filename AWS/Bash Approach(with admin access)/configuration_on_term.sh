cf861cf42dd65c0d27a2144676dab224b96130d8
broCode <56446798+PRASHANT-tech870@users.noreply.github.com>
2024-03-07 00:37:07 +0530

    Update configuration_on_term.sh

--- AWS/Bash Approach(with admin access)/configuration_on_term.sh
+++ AWS/Bash Approach(with admin access)/configuration_on_term.sh
@@ -1,36 +1,4 @@
 
-
-
-configure_s3cmd() {
-    local access_key="$1"
-    local secret_key="$2"
-    local region="$3"
-    local s3_endpoint="$4"
-    local bucket_template="$5"
-    local encryption_password="$6"
-    local gpg_program="$7"
-    local use_https="$8"
-    local http_proxy_name="$9"
-    local test="${10}"
-    local save="${11}"
-    local retry="${12}"
-
-    s3cmd --configure <<EOF
-$access_key
-$secret_key
-$region
-$s3_endpoint
-$bucket_template
-$encryption_password
-$gpg_program
-$use_https
-$http_proxy_name
-$test
-$save
-$retry
-EOF
-}
-
 configure_awscli() {
     local access_key="$1"
     local secret_key="$2"
@@ -47,8 +15,6 @@ EOF
 
 source /var/lib/HPCCSystems/mydropzone/configure_aws.sh
 
-
-configure_s3cmd "$ACCESS_KEY" "$SECRET_KEY" "$DEFAULT_REGION" "$S3_ENDPOINT" "$BUCKET_TEMPLATE" "$ENCRYPTION_PASSWORD" "$GPG_PROGRAM" "$USE_HTTPS" "$HTTP_PROXY_NAME" "$TEST_CASE" "$SAVE_SETTINGS" "$RETRY_CONFIGURATION"
 
 configure_awscli "$ACCESS_KEY" "$SECRET_KEY" "$DEFAULT_REGION" "$DEFAULT_OUTPUT"
 
